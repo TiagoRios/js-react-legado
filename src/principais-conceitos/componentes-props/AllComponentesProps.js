31d1f2ba5f08a471522bda0c9fc4722ea24717c1
Tiago Rios <tiago_pc9@hotmail.com>
2023-08-09 19:57:46 -0300

    AllComponentesProps.js renderiza todos os componentes da pasta que o contém

--- src/principais-conceitos/componentes-props/AllComponentesProps.js
+++ src/principais-conceitos/componentes-props/AllComponentesProps.js
@@ -5,33 +5,24 @@ import { PrimeiroComponent } from "./PrimeiroComponent";
 import { WelcomeWithClass } from "./WelcomeWithClass";
 import { WelcomeWithFunction } from "./WelcomeWithFunction";
 
-export class AllComponentesProps extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+export function AllComponentesProps() {
+    return (
+        <>
+            <h3>Componente Comments.js</h3>
+            <Comment author={autor111} text='algumm texto aqui asdf' date='2023' />
+            <hr />
 
-    render() {
-        return (
-            <div>
-                <h1>Componentes de principais conceitos</h1>
-                <h2>Pasta componentes-props</h2>
-     
-                <h3>Componente Comments.js</h3>
-                <Comment author={autor111} text='algumm texto aqui asdf' date='2023' />
-                <hr/>
+            <h3>Componente PrimeiroComponent.js</h3>
+            <PrimeiroComponent name="meuNome" />
+            <hr />
 
-                <h3>Componente PrimeiroComponent.js</h3>
-                <PrimeiroComponent name="meuNome"/>
-                <hr/>
+            <h3>Componente WelcomeWithClass.js</h3>
+            <WelcomeWithClass name="AlgumNome" description="AlgumaDescrição" />
+            <hr />
 
-                <h3>Componente WelcomeWithClass.js</h3>
-                <WelcomeWithClass name="AlgumNome" description="AlgumaDescrição"/>                
-                <hr/>
-
-                <h3>Componente WelcomeWithFunction.js</h3>
-                <WelcomeWithFunction name="AlgumNome" description="AlgumaDescrição"/>                
-                <hr/>
-            </div>
-        )
-    }
+            <h3>Componente WelcomeWithFunction.js</h3>
+            <WelcomeWithFunction name="AlgumNome" description="AlgumaDescrição" />
+            <hr />
+        </>
+    )
 }
