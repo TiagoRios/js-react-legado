6b223af71d76c0f109035eefd4a4865ef8440144
Tiago Rios <tiago_pc9@hotmail.com>
2023-08-09 20:07:24 -0300

    AllComposicaoHeranca.js renderiza todos componentes sobre composição herança

---
+++ src/principais-conceitos/composicao-heranca/AllComposicaoHeranca.js
@@ -0,0 +1,9 @@
+const AllComposicaoHeranca = () => {
+    return (<>
+        <h3>Componente Comments.js</h3>
+        <Comment author={autor111} text='algumm texto aqui asdf' date='2023' />
+        <hr />
+    </>)
+}
+
+export default AllComposicaoHeranca;
