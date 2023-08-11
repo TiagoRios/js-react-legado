9a3e6eb7eee9e7c9efb8d9cd73665c2c344d7a0e
Tiago Rios <tiago_pc9@hotmail.com>
2023-08-10 22:12:19 -0300

    MyApp.js utiliza divisão de código (code splitting)

--- src/guias-avancados/AllGuiasAvancados.js
+++ src/guias-avancados/AllGuiasAvancados.js
@@ -2,11 +2,13 @@ import React from "react"
 
 import AllAcessibility from "./acessibility/AllAcessibility"
 import TituloMaisToggle from "../utils-components/TituloMaisToggle"
+import { MyApp } from "./code-splitting/MyApp"
 
 export default class AllGuiasAvancados extends React.Component {
     render() {
         return (<>
             <TituloMaisToggle nome="Acessibilidade" componente={<AllAcessibility />} />
+            <TituloMaisToggle nome="Code Splitting" componente={<MyApp />} />
         </>)
     }
 }
