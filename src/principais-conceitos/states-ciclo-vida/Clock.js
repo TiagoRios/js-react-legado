b2eb8a83c73aa98491ba1531be9a526fa5ef3778
Tiago Rios <tiago_pc9@hotmail.com>
2023-12-09 23:13:51 -0300

    Refatoração - melhoria de código.

--- src/principais-conceitos/states-ciclo-vida/Clock.js
+++ src/principais-conceitos/states-ciclo-vida/Clock.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Titulo from "../../utils-components/Titulo";
+import Header from "../../utils-components/Header";
 
 /**
  * Componente React que representa um relógio e mostra as horas.
@@ -10,40 +10,33 @@ import Titulo from "../../utils-components/Titulo";
  * 
  * valores de this.props e this.state podem ser atualizados de forma assíncrona
  * não confie neles para calcular proximo state. use setstate((state, props)=>{}) ao 
- * inves da forma com objeto.
+ * invés da forma com objeto.
  *  
  * @see componentDidMount executa algo depois que renderizar componente no DOM.
  * @see componentWillUnmount limpar o DOM produzido quando componente for removido.
  */
 export default class Clock extends React.Component {
-    //Componentes de classe é obrigatório passar os PROPS no construtor
     constructor(props) {
         super(props);
-        //state e privado do React.
-        //No inicio, pega a hora e atribui ao state do componente
-        this.state = { date: new Date() };
+
+        this.state = {
+            date: new Date()
+        };
     }
 
-    //após renderizar na tela executa a cada segundo
     componentDidMount() {
-        this.temporizadorID = setInterval(// id do temporizador
-            //poderia ter passado a função diretamente. 
+        this.temporizadorID = setInterval(
             () => this.tick(),
-            // () => this.setState({
-            //     date: new Date()
-            // }),
-            1000 //tempo em milisegundos.
+            1000
         );
     }
 
-    //libera os recursos utilizados 
+    //Para libera os recursos utilizados.
     componentWillUnmount() {
         clearInterval(this.temporizadorID);
     }
 
-    //sempre que chamado atribui objeto Date() ao atributo date de Clock.
     tick() {
-        //Essa é a forma correta.
         //Não altere o state diretamente por ex: this.state.date = new Date();
         this.setState({
             date: new Date()
@@ -51,12 +44,13 @@ export default class Clock extends React.Component {
     }
 
     render() {
-        return (<div style={{ border: '5px solid brown' }}>
-            <Titulo title="Clock.js" component={<>
+        return (
+            <div style={{ border: '5px solid brown' }}>
+                <Header title="Clock.js" />
+
                 <h1>Relógio!</h1>
-                {/* usando o state ao inves do props */}
                 <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </>} />
-        </div>);
+            </div>
+        );
     }
 }
