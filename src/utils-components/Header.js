10acbb654f61deecbeacdede043377ae280fe7ba
Tiago Rios <tiago_pc9@hotmail.com>
2023-12-12 23:45:16 -0300

    Refatoração - Adicionado mais opções de nível header.

--- src/utils-components/Header.js
+++ src/utils-components/Header.js
@@ -1,14 +1,30 @@
 import HeaderH2 from "./HeaderH2";
 
-export default function Header({ title, level = 2 }) {
+export default function Header({ title, level }) {
     switch (level) {
+        case 1:
+            return (
+                <h1 style={{ color: "yellow" }}>
+                    {title}
+                </h1>
+            )
         case 2:
             return (
-                <HeaderH2 color="lightgreen">
+                <HeaderH2 color="orange">
                     {title}
                 </HeaderH2>
             )
+        case 3:
+            return (
+                <h3 style={{ color: "magenta" }}>
+                    {title}
+                </h3>
+            )
         default:
-            return <h1>{title}</h1>
+            return (
+                <HeaderH2 color="orange">
+                    {title}
+                </HeaderH2>
+            )
     }
 }
