f7a627d3f6baa943583dd8a3e0193089da225e1e
Tiago Rios <tiago_pc9@hotmail.com>
2023-12-14 21:45:41 -0300

    Refatoração - Renderiza os errors boundaries.

--- src/guias-avancados/guiasAvancadosList.js
+++ src/guias-avancados/guiasAvancadosList.js
@@ -1,11 +1,13 @@
 import AllAcessibility from "./acessibility/AllAcessibility"
 import AllContext from "./context/AllContext"
 import CodeSplitting from "./code-splitting/CodeSplitting"
+import AllErrorBoundaries from "./error-boundaries/AllErrorBoundaries"
 
 const guiasAvancadosList = [
   { titulo: 'Acessibilidade', componente: <AllAcessibility /> },
   { titulo: 'Code Splitting', componente: <CodeSplitting /> },
   { titulo: 'Context', componente: <AllContext /> },
+  { titulo: 'Error Boundaries', componente: <AllErrorBoundaries /> },
 ]
 
 export default guiasAvancadosList;
