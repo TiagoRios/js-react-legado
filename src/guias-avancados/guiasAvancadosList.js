7623064f1e311792cf8903393668dc72db4a0880
Tiago Rios <tiago_pc9@hotmail.com>
2023-12-14 22:54:44 -0300

    Novo conteúdo adicionado a lista.

--- src/guias-avancados/guiasAvancadosList.js
+++ src/guias-avancados/guiasAvancadosList.js
@@ -2,12 +2,14 @@ import AllAcessibility from "./acessibility/AllAcessibility"
 import AllContext from "./context/AllContext"
 import CodeSplitting from "./code-splitting/CodeSplitting"
 import AllErrorBoundaries from "./error-boundaries/AllErrorBoundaries"
+import AllForwarRef from "./forward-refs/AllForwarRef"
 
 const guiasAvancadosList = [
   { titulo: 'Acessibilidade', componente: <AllAcessibility /> },
   { titulo: 'Code Splitting', componente: <CodeSplitting /> },
   { titulo: 'Context', componente: <AllContext /> },
   { titulo: 'Error Boundaries', componente: <AllErrorBoundaries /> },
+  { titulo: 'Forward ref', componente: <AllForwarRef /> },
 ]
 
 export default guiasAvancadosList;
